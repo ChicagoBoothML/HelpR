b1aeadfc7d2aa91c146052f89f1288b8784ec0a8
MBA Learns to Code <MBALearnsToCode@UChicago.edu>
2015-10-16 16:55:08 -0500

    add ClassfierEvaluation

---
+++ ClassifierEvaluation.R
@@ -0,0 +1,81 @@
+bin_classif_eval_hard_pred <- function(hard_predictions, actuals) {
+  if (is.factor(hard_predictions)) {
+    hard_predictions <- as.integer(hard_predictions)
+  }
+  if (is.integer(hard_predictions)) {
+    hard_predictions <- hard_predictions - min(hard_predictions)
+  }
+  
+  if (is.factor(actuals)) {
+    actuals <- as.integer(actuals)
+  }
+  if (is.integer(actuals)) {
+    actuals <- actuals - min(actual)
+  }
+  
+  opposite_hard_predictions <- ! hard_predictions
+  opposite_actuals <- ! actuals
+  
+  nb_samples <- length(actuals)
+  nb_pos <- sum(actuals)
+  nb_neg <- sum(opposite_actuals)
+  nb_pred_pos <- sum(hard_predictions)
+  nb_pred_neg <- sum(opposite_hard_predictions)
+  nb_true_pos <- sum(hard_predictions * actuals)
+  nb_true_neg <- sum(opposite_hard_predictions * opposite_actuals)
+  nb_false_pos <- sum(hard_predictions * opposite_actuals)
+  nb_false_neg <- sum(opposite_hard_predictions * actuals)
+  
+  accuracy <- (nb_true_pos + nb_true_neg) / nb_samples
+  sensitivity <- nb_true_pos / nb_pos
+  specificity <- nb_true_neg / nb_neg
+  precision <- nb_true_pos / nb_predicted_pos
+  f1_score <- (2 * precision * sensitivity) / (precision + sensitivity)
+  
+  c(accuracy=accuracy,
+    sensitivity=sensitivity,
+    specificity=specificity,
+    precision=precision,
+    f1_score=f1_score)
+}
+
+
+bin_classif_eval <- function(predictions, actuals, thresholds=.5) {
+  
+  if (is.factor(predictions) || is.integer(predictions) || is.logical(predictions)) {
+    bin_classif_eval_hard_pred(predictions, actuals)
+  } else {
+    
+    if (length(thresholds) == 1) {
+      
+      hard_predictions <- predictions > thresholds
+      
+      source('https://raw.githubusercontent.com/ChicagoBoothML/HelpR/master/CostFunctions.R')
+      
+      c(bin_classif_eval_hard_pred(hard_predictions, actuals),
+        c(deviance=bin_class_dev(predictions, actuals)))
+      
+    } else {
+      
+      library(data.table)
+      
+      d <- data.table(threshold=thresholds)
+      column_names <- c(
+        'accuracy',
+        'sensitivity',
+        'specificity',
+        'precision',
+        'f1_score',
+        'deviance'
+      )
+      d[, column_names := as.list(rep(0, times=6))]
+      
+      for (i in seq.along(thresholds)) {
+        d[i, column_names := as.list(
+          bin_classif_eval(predictions, actuals, thresholds=thresholds[i]))]
+      }
+      
+      d
+    }
+  }
+}
