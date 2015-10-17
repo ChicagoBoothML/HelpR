9ad5cf8a9a8e4c784981a46d9f1091cbac5f1db6
MBA Learns to Code <MBALearnsToCode@UChicago.edu>
2015-10-16 21:00:40 -0500

    fix typos

--- EvaluationMetrics.R
+++ EvaluationMetrics.R
@@ -89,10 +89,10 @@ bin_classif_eval <- function(predictions, actuals, thresholds=.5) {
         'f1_score',
         'deviance'
       )
-      d[, column_names := as.list(rep(0, times=6))]
+      d[, (column_names) := as.list(rep(0, times=6))]
       
       for (i in seq_along(thresholds)) {
-        d[i, column_names := as.list(
+        d[i, (column_names) := as.list(
           bin_classif_eval(predictions, actuals, thresholds=thresholds[i]))]
       }
       
