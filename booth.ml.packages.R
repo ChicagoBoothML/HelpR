a3434798fd401a399bc7d9fdc6f8bc39ca8ac012
Mladen Kolar <mkolar@gmail.com>
2015-10-25 20:01:12 -0500

    lift curve utility functions

---
+++ booth.ml.packages.R
@@ -0,0 +1,45 @@
+
+packageNames = c("MASS", 
+                 "ISLR",
+                 "animation",
+                 "ElemStatLearn",
+                 "glmnet",
+                 "textir",
+                 "nnet",
+                 "methods",
+                 "statmod",
+                 "stats",
+                 "graphics",
+                 "RCurl",
+                 "jsonlite",
+                 "tools",
+                 "utils",
+                 "data.table",
+                 "gbm",
+                 "ggplot2",
+                 "randomForest",
+                 "tree",
+                 "class",
+                 "kknn"
+                 )
+
+
+for (pkgName in packageNames) {
+  if (! (pkgName %in% rownames(installed.packages()))) { 
+    install.packages(pkgName,
+                     dependencies=TRUE,
+                     repos='http://cran.rstudio.com') 
+  }  
+}
+
+
+if (! ("h20" %in% rownames(installed.packages()))) { 
+  # Now we download, install and initialize the H2O package for R.
+  if (packageVersion("h2o") < "3.5.0.3232") {
+    install.packages("h2o", 
+                     type="source", 
+                     repos=(c("http://h2o-release.s3.amazonaws.com/h2o/master/3232/R"))
+                     )
+  }
+}
+
---
+++ lift.R
@@ -0,0 +1,34 @@
+
+# plots one lift curve
+lift.plot = function(phat, y.list) {
+  if(is.factor(y.list)) y.list = as.numeric(y.list)-1
+  oo = order(-phat)
+  sy = cumsum(y.list[oo])/sum(y.list==1)
+  
+  ii = (1:length(sy))/length(sy)
+  plot(ii, sy, 
+       type='l', lwd=2, col='blue', 
+       xlab='% tried',ylab='% of successes',
+       cex.lab=2
+       )
+  abline(0, 1, lty=2)
+}
+
+
+# plots multiple lift curves
+# phat.list is a list 
+# each element of the list is a vector phat for different model
+lift.many.plot = function(phat.list, y.list) {
+  if(is.factor(y.list)) y.list = as.numeric(y.list)-1
+  n = length(y.list)
+  
+  par(mai=c(.8, 1.2, .5, .5))
+  plot(c(0,1), c(0,1), type="n", xlab='% tried', ylab='% of successes', cex.lab=2)
+  abline(0, 1, lty=2)
+  ii = (1:n) / n
+  for(i in 1:length(phat.list)) {
+    oo = order(-phat.list[[i]])
+    sy = cumsum(y.list[oo])/sum(y.list==1)
+    lines(ii,sy,type="l",lwd=2,col=i)
+  }
+}
