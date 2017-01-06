8b376f22d3ad232bf45ae8ec8d42274cf38ff625
Mladen Kolar <mkolar@gmail.com>
2017-01-05 20:45:45 -0600

    Update docv.R

--- docv.R
+++ docv.R
@@ -1,3 +1,33 @@
+# DESCRIPTION:
+# Lets us run a k-fold cross validation on a k-nearest neighbor analysis. Or in technical terms: “docvknn is for kNN, it calls docv handing in a wrapper of kknn.” (not quite sure what that means).
+#  
+# Importantly, the function returns data in the Sum of Squares format, so if you want to plot it, you’ll want to correct for this (see “Usage” below)
+# 
+# 
+# USAGE:
+# 
+# To do 10-fold cross validation on how our dataset Xdata predicts for Ydata, across the range of possible k-nearest neighbors of 1-100:
+# 
+# Krange = 1:100
+# newcv = docvkkn(Xdata, Ydata, Krange, nfold=10)
+# 
+# Or to see an example from the course notes (For more details on this usage example, see slide 78 of 01_knn.pdf):
+# cvtemp = docvknn(xs, y, kv, nfold=10)
+#  
+# In order to optimally plot this data you will want to get the data out of Sum of Squares format:
+# cvtemp = sqrt(cvtemp/(length(y))
+#   
+# ARGUMENTS:
+# x = the variables that are your regressors/covariates (takes dataframe, matrix or vector object)
+# y = the variable what we want to predict (takes dataframe, matrix or vector object)
+# k = a range of possible values for how many neighbors that we want to test (our “tuning knob”). (It definitely takes a vector, I don't know about anything else)
+# nfold = number of folds in your cross validation (usually 5 or 10, but you can plug in whatever you want)
+#  
+# Optional arguments
+# doran = ??? (If omitted from the function when it is called, docvkkn works just find, so probably just leave this one alone)
+# verbose = indicates the amount of description included in the output (Defaults to verbose mode if omitted from the function when it is called)
+# 
+
 ############################################################
 ############################################################
 ## Function to do cross validation.
